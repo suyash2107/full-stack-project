c13bb170b182b9d7557d8f9b252c69a964261b34
suyash <suyashshinde76@gmail.com>
2024-04-08 00:40:19 +0530

    port change

--- server/index.js
+++ server/index.js
@@ -218,7 +218,7 @@ if(process.env.NODE_ENV === "production"){
 }
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`);
