30aecce51ede7498227429b844206e2a062dc823
suyash <suyashshinde76@gmail.com>
2024-04-08 00:49:13 +0530

    error solved of server crash

--- server/index.js
+++ server/index.js
@@ -218,7 +218,7 @@ if(process.env.NODE_ENV === "production"){
 }
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`);
